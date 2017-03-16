98e8376da7f9f815132568b9569df42fce5b0802
Michael Rogov <michael@e8storage.com>
2017-03-16 17:30:20 +0200

    added timestamp to backup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 echo "backup current configs"
 mkdir $(pwd)/backup
-mv ~/.vimrc $(pwd)/backup/vimrc.orig
-mv ~/.gitconfig $(pwd)/backup/gitconfig.orig
-mv ~/.tmux.conf $(pwd)/backup/tmux.conf.orig
+mv ~/.vimrc $(pwd)/backup/vimrc$(date -I'seconds')
+mv ~/.gitconfig $(pwd)/backup/gitconfig$(date -I'seconds')
+mv ~/.tmux.conf $(pwd)/backup/tmux.conf$(date -I'seconds')
 echo "making symlinks"
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
@@ -10,3 +10,4 @@ ln -s $(pwd)/tmux.conf ~/.tmux.conf
 echo "updating plugins"
 . ./update.sh
 echo "add to your .bashrc - \". $(pwd)/bashrc\""
+
