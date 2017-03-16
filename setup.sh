3d8acbac991a3cc806063d57af7c265b1ff491eb
Michael Rogov <michael@e8storage.com>
2017-03-16 17:10:46 +0200

    added mkdir backup in setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 echo "backup current configs"
-mv ~/.vimrc backup/vimrc.orig
-mv ~/.gitconfig backup/gitconfig.orig
-mv ~/.tmux.conf backup/tmux.conf.orig
+mkdir $(pwd)/backup
+mv ~/.vimrc $(pwd)/backup/vimrc.orig
+mv ~/.gitconfig $(pwd)/backup/gitconfig.orig
+mv ~/.tmux.conf $(pwd)/backup/tmux.conf.orig
 echo "making symlinks"
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
